2e5741cb856778a6b5a21ddcb9fa1d32ae6d132a
Abed <abdulkareem.hajqasem@gmail.com>
2023-11-05 00:51:56 +0200

    finsihed

--- cypress/support/pageObjects/mainPages/addEmployeePage.ts
+++ cypress/support/pageObjects/mainPages/addEmployeePage.ts
@@ -1,28 +1,121 @@
-class addEmployeePage{
+import GenericFunctions from "../../../../cypress/e2e/conduit/support/genericFunctions";
+class AddEmployeePage {
     elements = {
-        saveBTN: () => cy.get('button').contains('Save'),
-        CancelBTN: () => cy.get('button').contains('Cancel'),
-        firstName: () => cy.get('[placeholder = "First Name"]'),
-        middleName: () => cy.get('[placeholder = "Middle Name"]'),
-        lastName: () => cy.get('[placeholder = "Last Name"]'),
-        employeeId: () => cy.get(':nth-child(1) > .oxd-grid-2 > .oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input'),
-        loginDetailsSwitch: () => cy.get('.oxd-switch-input'),
-        userName: () => cy.get(':nth-child(4) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input'),
-        password: () => cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input'),
-        confirmPassword: () => cy.get('.oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input')
+      saveButton: () => cy.get("button").contains("Save"),
+      cancelButton: () => cy.get("button").contains("Cancel"),
+      firstNameInput: () => cy.get('[placeholder = "First Name"]'),
+      middleNameInput: () => cy.get('[placeholder = "Middle Name"]'),
+      lastNameInput: () => cy.get('[placeholder = "Last Name"]'),
+      employeeIdInput: () =>
+        cy.get(
+          ":nth-child(1) > .oxd-grid-2 > .oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input"
+        ),
+      loginDetailsSwitch: () => cy.get(".oxd-switch-input"),
+      usernameInput: () =>
+        cy.get(
+          ":nth-child(4) > .oxd-grid-2 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input"
+        ),
+      passwordInput: () =>
+        cy.get(
+          ".user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input"
+        ),
+      confirmPasswordInput: () =>
+        cy.get(
+          ".oxd-grid-2 > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input"
+        ),
+    };
+  
+    urls = {
+      employees: "/web/index.php/api/v2/pim/employees",
+      users: "/web/index.php/api/v2/admin/users",
+    };
+  
+    addWithLogin = (
+      firstName: string,
+      middleName: string,
+      lastName: string,
+      username: string,
+      password: string
+    ) => {
+      this.actions.enterFirstName(firstName);
+      this.actions.enterMiddleName(middleName);
+      this.actions.enterLastName(lastName);
+      this.actions.addLoginSwitch();
+      this.actions.enterUsername(username);
+      this.actions.enterPassword(password);
+      this.actions.enterConfirmPassword(password);
+      this.actions.clickSaveButton();
+    };
+  
+    actions = {
+      enterFirstName: (firstName: string) =>
+        this.elements.firstNameInput().type(firstName),
+      enterMiddleName: (middleName: string) =>
+        this.elements.middleNameInput().type(middleName),
+      enterLastName: (lastName: string) =>
+        this.elements.lastNameInput().type(lastName),
+      addLoginSwitch: () => this.elements.loginDetailsSwitch().click(),
+      enterUsername: (username: string) =>
+        this.elements.usernameInput().type(username),
+      enterPassword: (password: string) =>
+        this.elements.passwordInput().type(password),
+      enterConfirmPassword: (password: string) =>
+        this.elements.confirmPasswordInput().type(password),
+      clickSaveButton: () => this.elements.saveButton().click(),
+    };
+  
+    addEmployeeUsingAPI(employeeData: any) {
+      const employeePayload = {
+        firstName: employeeData.firstName,
+        middleName: employeeData.middleName,
+        lastName: employeeData.lastName,
+        empPicture: null,
+        employeeId:`${GenericFunctions.genericRandomNumber(1000)}`,
+      };
+  
+      return cy.api({
+        method: "POST",
+        url: this.urls.employees,
+        body: employeePayload,
+      });
     }
-
-
-    addWithLogin = (firstName: string, middleName: string, lastName: string, userName: string, password: string) => {
-        this.elements.firstName().type(firstName);
-        this.elements.middleName().type(middleName);
-        this.elements.lastName().type(lastName);
-        this.elements.loginDetailsSwitch().click();
-        this.elements.userName().type(userName);
-        this.elements.password().type(password);
-        this.elements.confirmPassword().type(password);
-        this.elements.saveBTN().click();
+  
+    addUserWithLoginUsingAPI(employeeData: any) {
+      return this.addEmployeeUsingAPI(employeeData).then((employeeResponse) => {
+        const empNo = employeeResponse.body.data.empNumber;
+  
+        const userPayload = {
+          username: employeeData.username,
+          password: employeeData.password,
+          status: true,
+          userRoleId: 2,
+          empNumber: empNo,
+        };
+  
+        return cy
+          .api({
+            method: "POST",
+            url: this.urls.users,
+            body: userPayload,
+          })
+          .then((userResponse) => {
+            console.log(userResponse);
+            return userResponse.body;
+          });
+      });
     }
-}
-
-export default addEmployeePage
+  
+    deleteEmployee(id: number) {
+      const deletePayload = {
+        ids: [id],
+      };
+  
+      return cy.api({
+        method: "DELETE",
+        url: this.urls.employees,
+        body: deletePayload,
+      });
+    }
+  }
+  export default AddEmployeePage;
+  
---
+++ cypress/support/pageObjects/objects/table.ts
@@ -0,0 +1,46 @@
+class table {
+    private columns: Array<string> = new Array();
+  
+    create(columns: Array<string>) {
+      this.columns = columns;
+    }
+  
+    getNumOfRows(url: string) {
+      return cy
+        .api({
+          url: url,
+          method: "GET",
+          body: {},
+        })
+        .its("body")
+        .its("meta")
+        .its("total");
+    }
+  
+    getCellWithSelect(row: number, attr: string) {
+      let ind = this.columns.indexOf(attr);
+  
+      return cy.get(
+        `div.oxd-table-body > div:nth-child(${row}) > div > div:nth-child(${
+          ind + 2
+        }) > div`
+      );
+    }
+  
+    getCellWithoutSelect(row: number, attr: string) {
+      let ind = this.columns.indexOf(attr);
+  
+      return cy.get(
+        `div.oxd-table-body > div:nth-child(${row}) > div > div:nth-child(${
+          ind + 1
+        }) > div`
+      );
+    }
+  
+    checkValue(row: number, attr: string, expected: any) {
+      this.getCellWithSelect(row, attr).should("contain.text", expected);
+    }
+  }
+  
+  export default table;
+  
